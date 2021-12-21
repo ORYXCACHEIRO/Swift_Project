380cf7d813b46a5f4f2c7d1ded824c67166764d9
Daniel Faria <danieldosfarias@gmail.com>
2021-12-21 12:27:09 +0000

    tables part 1

---
+++ swift_pro/swift_pro/Controllers/NewsTableController.swift
@@ -0,0 +1,25 @@
+//
+//  NewsTableController.swift
+//  swift_pro
+//
+//  Created by ci on 21/12/2021.
+//
+
+// seguir este exemplo
+//https://www.ralfebert.com/ios-examples/uikit/uitableviewcontroller/
+
+import UIKit
+
+struct Article {
+    var title: String
+    var details: String
+    var image: String
+    var id: Int
+}
+
+
+class NewsTableController: UITableViewController {
+
+    
+
+}
