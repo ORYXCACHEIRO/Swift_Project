28c5e95ea187c977b45b35df8b866779f72ecdfc
Daniel Faria <danieldosfarias@gmail.com>
2021-12-21 15:42:11 +0000

    coisas

--- swift_pro/swift_pro/Controllers/NewsTableController.swift
+++ swift_pro/swift_pro/Controllers/NewsTableController.swift
@@ -6,7 +6,7 @@
 //
 
 // seguir este exemplo
-//https://www.ralfebert.com/ios-examples/uikit/uitableviewcontroller/
+//https://levelup.gitconnected.com/getting-started-with-uitableview-in-swift-9b04e6fd8b9a
 
 import UIKit
 
@@ -14,12 +14,37 @@ struct Article {
     var title: String
     var details: String
     var image: String
-    var id: Int
 }
 
+class NewsTableController: ViewController, UITableViewDelegate, UITableViewDataSource {
 
-class NewsTableController: UITableViewController {
+    let articles = [
+        Article(title: "Noticia", details: "Austria", image: "https://cdn.ralfebert.de/static_table_contents-719d31f2.png"),
+        Article(title: "Noticia", details: "Belgium", image: "https://cdn.ralfebert.de/static_table_contents-719d31f2.png"),
+        Article(title: "Noticia", details: "Germany", image: "https://cdn.ralfebert.de/static_table_contents-719d31f2.png"),
+        Article(title: "Noticia", details: "Greece", image: "https://cdn.ralfebert.de/static_table_contents-719d31f2.png"),
+        Article(title: "Noticia", details: "France", image: "https://cdn.ralfebert.de/static_table_contents-719d31f2.png"),
+    ]
 
+    @IBOutlet weak var tableOut: UITableView!
+    
+    // MARK: - Table view data source
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableOut.delegate = self
+        tableOut.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
     
 
 }
